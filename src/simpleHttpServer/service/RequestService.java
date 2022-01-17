08e67620e5359c3232a6ed60c1927ce119532a44
JakubLeonik <leonikjakub@gmail.com>
2022-01-17 12:28:44 +0100

    Add finding resource

--- src/simpleHttpServer/service/RequestService.java
+++ src/simpleHttpServer/service/RequestService.java
@@ -2,8 +2,11 @@ package simpleHttpServer.service;
 
 import simpleHttpServer.model.Request;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 public class RequestService {
@@ -15,12 +18,25 @@ public class RequestService {
 
     public void serve(Socket requestSocket) throws IOException {
         Request request = new Request(requestSocket);
+        initializeRequest(request);
+        byte[] resource = findResource(request.getPath());
+        if(resource == null) sendResponse(request, "404 Not Found", resource);
+        else sendResponse(request, "200 OK", resource);
+        request.end();
+    }
+
+    private byte[] findResource(Path path) throws IOException {
+        if(Files.exists(path)) return Files.readAllBytes(path);
+        else return null;
+    }
+
+    private void initializeRequest(Request request) throws IOException {
         List<String> requestContent = request.getInputContent();
         String[] requestLine = requestContent.get(0).split(" ");
         request.setMethod(requestLine[0]);
         request.setPath(rootDirectoryPath+requestLine[1]);
-        request.end();
-        System.out.println(requestContent);
     }
-
+    private void sendResponse(Request request, String status, byte[] resource){
+        System.out.println("I will send answer with status: "+status);
+    }
 }
